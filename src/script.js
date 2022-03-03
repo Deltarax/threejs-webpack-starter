5cc0a25e87d61f700a5fc4047ec92f456a0ca988
Jackson <deltarax@gmail.com>
2022-03-03 15:38:38 -0800

    added more spheres

--- src/script.js
+++ src/script.js
@@ -43,7 +43,7 @@ scene.background = bgTexture;
 // const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 const sphereGeometry = new THREE.SphereBufferGeometry(.7, 64, 64)
 const planeGeometry = new THREE.BoxGeometry(50, .01, 50);
-const pyramidGeometry = new THREE.ConeGeometry( 7, 7, 4 );
+const pyramidGeometry = new THREE.ConeGeometry( 7, 7, 4, 7 );
 const pillarGeometry = new THREE.CylinderGeometry( .5, .5, 4, 8);
 const pillarBaseGeometry = new THREE.BoxGeometry(1.2, .25, 1.2);
 
@@ -75,7 +75,7 @@ var mtlLoader = new MTLLoader();
         } );
     // rock08.position.x = -12
     rock08.scale.set(1.2, 1.2, 1.2)
-    rock08.position.y = -5.2
+    rock08.position.y = -5.4
     
     // rock08.position.z = -12
     scene.add(rock08);
@@ -120,10 +120,26 @@ const pillarBaseMat = new THREE.MeshStandardMaterial({color: 0xCDAA6D})
 
 // Mesh
 const blackSphere = new THREE.Mesh(sphereGeometry,blackSphereMat)
+const blackSphere01 = new THREE.Mesh(sphereGeometry,blackSphereMat)
+const blackSphere02 = new THREE.Mesh(sphereGeometry,blackSphereMat)
+const blackSphere03 = new THREE.Mesh(sphereGeometry,blackSphereMat)
+const blackSphere04 = new THREE.Mesh(sphereGeometry,blackSphereMat)
 const plane = new THREE.Mesh(planeGeometry, planeMaterial)
 const pyramid = new THREE.Mesh( pyramidGeometry, pyramidMaterial );
 scene.add(plane)
 scene.add(blackSphere)
+scene.add(blackSphere01)
+scene.add(blackSphere02)
+scene.add(blackSphere03)
+scene.add(blackSphere04)
+blackSphere01.scale.set(.5, .5, .5)
+blackSphere02.scale.set(.5, .5, .5)
+blackSphere03.scale.set(.5, .5, .5)
+blackSphere04.scale.set(.5, .5, .5)
+blackSphere01.position.set(-1.5, 0, 0)
+blackSphere02.position.set(1.5, 0, 0)
+blackSphere03.position.set(0, .5, -1.5)
+blackSphere04.position.set(0, -.5, 1.5)
 scene.add(pyramid)
 pyramid.scale.set(.4, .4, .4)
 pyramid.rotation.y = Math.PI/4
@@ -159,6 +175,118 @@ pillarTop02.position.set(6, -3.4, 10)
 pillar02.scale.set(.4, .4, .4)
 pillarBot02.scale.set(.4, .4, .4)
 pillarTop02.scale.set(.4, .4, .4)
+
+const pillar03 = new THREE.Mesh(pillarGeometry,pillarMat)
+scene.add(pillar03)
+pillar03.scale.set(.2, .2, .2)
+pillar03.position.set(6, -4.2, 8)
+const pillarBot03 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarBot03)
+pillarBot03.position.set(6, -4.95, 8)
+const pillarTop03 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarTop03)
+pillarTop03.position.set(6, -3.4, 8)
+pillar03.scale.set(.4, .4, .4)
+pillarBot03.scale.set(.4, .4, .4)
+pillarTop03.scale.set(.4, .4, .4)
+
+const pillar04 = new THREE.Mesh(pillarGeometry,pillarMat)
+scene.add(pillar04)
+pillar04.scale.set(.2, .2, .2)
+pillar04.position.set(6, -4.2, 14)
+const pillarBot04 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarBot04)
+pillarBot04.position.set(6, -4.95, 14)
+const pillarTop04 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarTop04)
+pillarTop04.position.set(6, -3.4, 14)
+pillar04.scale.set(.4, .4, .4)
+pillarBot04.scale.set(.4, .4, .4)
+pillarTop04.scale.set(.4, .4, .4)
+
+const pillar05 = new THREE.Mesh(pillarGeometry,pillarMat)
+scene.add(pillar05)
+pillar05.scale.set(.2, .2, .2)
+pillar05.position.set(6, -4.2, 16)
+const pillarBot05 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarBot05)
+pillarBot05.position.set(6, -4.95, 16)
+const pillarTop05 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarTop05)
+pillarTop05.position.set(6, -3.4, 16)
+pillar05.scale.set(.4, .4, .4)
+pillarBot05.scale.set(.4, .4, .4)
+pillarTop05.scale.set(.4, .4, .4)
+
+const pillar06 = new THREE.Mesh(pillarGeometry,pillarMat)
+scene.add(pillar06)
+pillar06.scale.set(.2, .2, .2)
+pillar06.position.set(-6, -4.2, 12)
+const pillarBot06 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarBot06)
+pillarBot06.position.set(-6, -4.95, 12)
+const pillarTop06 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarTop06)
+pillarTop06.position.set(-6, -3.4, 12)
+pillar06.scale.set(.4, .4, .4)
+pillarBot06.scale.set(.4, .4, .4)
+pillarTop06.scale.set(.4, .4, .4)
+
+const pillar07 = new THREE.Mesh(pillarGeometry,pillarMat)
+scene.add(pillar07)
+pillar07.scale.set(.2, .2, .2)
+pillar07.position.set(-6, -4.2, 10)
+const pillarBot07 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarBot07)
+pillarBot07.position.set(-6, -4.95, 10)
+const pillarTop07 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarTop07)
+pillarTop07.position.set(-6, -3.4, 10)
+pillar07.scale.set(.4, .4, .4)
+pillarBot07.scale.set(.4, .4, .4)
+pillarTop07.scale.set(.4, .4, .4)
+
+const pillar08 = new THREE.Mesh(pillarGeometry,pillarMat)
+scene.add(pillar08)
+pillar08.scale.set(.2, .2, .2)
+pillar08.position.set(-6, -4.2, 8)
+const pillarBot08 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarBot08)
+pillarBot08.position.set(-6, -4.95, 8)
+const pillarTop08 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarTop08)
+pillarTop08.position.set(-6, -3.4, 8)
+pillar08.scale.set(.4, .4, .4)
+pillarBot08.scale.set(.4, .4, .4)
+pillarTop08.scale.set(.4, .4, .4)
+
+const pillar09 = new THREE.Mesh(pillarGeometry,pillarMat)
+scene.add(pillar09)
+pillar09.scale.set(.2, .2, .2)
+pillar09.position.set(-6, -4.2, 14)
+const pillarBot09 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarBot09)
+pillarBot09.position.set(-6, -4.95, 14)
+const pillarTop09 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarTop09)
+pillarTop09.position.set(-6, -3.4, 14)
+pillar09.scale.set(.4, .4, .4)
+pillarBot09.scale.set(.4, .4, .4)
+pillarTop09.scale.set(.4, .4, .4)
+
+const pillar10 = new THREE.Mesh(pillarGeometry,pillarMat)
+scene.add(pillar10)
+pillar10.scale.set(.2, .2, .2)
+pillar10.position.set(-6, -4.2, 16)
+const pillarBot10 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarBot10)
+pillarBot10.position.set(-6, -4.95, 16)
+const pillarTop10 = new THREE.Mesh(pillarBaseGeometry,pillarBaseMat)
+scene.add(pillarTop10)
+pillarTop10.position.set(-6, -3.4, 16)
+pillar10.scale.set(.4, .4, .4)
+pillarBot10.scale.set(.4, .4, .4)
+pillarTop10.scale.set(.4, .4, .4)
 }
 
 
@@ -171,6 +299,7 @@ dbPyramid.add(pyramid.position, 'z').min(-3).max(3).step(0.01)
 
 // Lights
 
+{
 // Light 1
 const pointLight = new THREE.PointLight(0xffffff, 0.1)
 pointLight.position.x = 2
@@ -180,7 +309,7 @@ pointLight.position.z = 4
 
 // Light 2
 const pointLight2 = new THREE.PointLight(0xff0000, 2)
-pointLight2.position.set(-3.48, -3, -1.6)
+pointLight2.position.set(0, -1.65, 0)
 pointLight2.intensity = .6
 scene.add(pointLight2)
 
@@ -196,7 +325,7 @@ light1.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
 
 // Light 3
 const pointLight3 = new THREE.PointLight(0xD2AA6D, 2)
-pointLight3.position.set(3, -3, 1.46)
+pointLight3.position.set(0, -3, 0)
 pointLight3.intensity = 1.26
 scene.add(pointLight3)
 
@@ -226,6 +355,8 @@ scene.add(sunLight);
 
 const ambientLight = new THREE.AmbientLight( 0x000000 );
 scene.add(ambientLight);
+}
+
 
 /**
  * Sizes
@@ -321,6 +452,10 @@ const tick = () =>
 
     // Update objects
     blackSphere.rotation.y = .5 * elapsedTime
+    blackSphere01.rotation.x = -.5 * elapsedTime
+    blackSphere02.rotation.z = -.5 * elapsedTime
+    blackSphere03.rotation.z = .5 * elapsedTime
+    blackSphere04.rotation.x = .5 * elapsedTime
     // rock08.rotation.y = .5 * elapsedTime
 
     //Manual Sphere Rotation
